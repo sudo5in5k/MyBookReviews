a0bc7118f12ce40e8d3679e6cac1c7fcd364c51f
EC2 Default User <ec2-user@ip-172-31-45-148.us-east-2.compute.internal>
2019-07-16 15:34:08 +0000

    first view

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  
+  get 'posts/index'
+
+  # posts
+  get "posts/index" => "posts#index"
+  get "posts/:id" => "posts#show"
+  
+  # home
+  get "/home/top" => "home#top"
+  get "/about" => "home#about"
+  get "/" => "home#top"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
